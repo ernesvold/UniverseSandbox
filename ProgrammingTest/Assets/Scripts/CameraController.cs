937e991b61c39590aaaaaa177a90dbb772e2b4b5
ernesvold <erikanesvold@Erikas-MacBook-Pro.local>
2017-09-08 13:52:13 -0400

    Added CameraController script
    - Also made camera’s initial position depend on box size

---
+++ ProgrammingTest/Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	void Start () {
+		float boxsize = GameObject.Find ("MainGameObject").GetComponent<MainScript> ().boxsize;
+		transform.position = new Vector3 (0, 0, -4 * boxsize);
+	}
+	
+
+	void Update () {
+		// TODO: User-controlled camera
+	}
+}
