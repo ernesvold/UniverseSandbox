b07d845fead48c1ff82ca120c2e6b699effbe047
ernesvold <erikanesvold@Erikas-MacBook-Pro.local>
2017-09-14 21:12:37 -0400

    Added user controls to camera

--- ProgrammingTest/Assets/Scripts/CameraController.cs
+++ ProgrammingTest/Assets/Scripts/CameraController.cs
@@ -4,13 +4,50 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
+	private float zoomspeed = 5.0f; // speed of camera zoom
+	private float angularspeed = 0.5f; // speed of camera rotation
+
 	void Start () {
+		// Move camera back far enough to see entire box
 		float boxsize = GameObject.Find ("MainGameObject").GetComponent<MainScript> ().boxsize;
 		transform.position = new Vector3 (0, 0, -4 * boxsize);
 	}
 	
 
 	void Update () {
-		// TODO: User-controlled camera
+
+		// Zoom in and out with W and S
+		if (Input.GetKey (KeyCode.W)) {
+			Vector3 zoom = transform.position.normalized*zoomspeed*Time.deltaTime;
+			transform.position -= zoom;
+		}
+
+		if (Input.GetKey (KeyCode.S)) {
+			Vector3 zoom = transform.position.normalized*zoomspeed*Time.deltaTime;
+			transform.position += zoom;
+		}
+
+		// Rotate around y-axis with left and right arrow keys
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			float angle = -angularspeed*Time.deltaTime; // angle to rotate through (rad)
+			float sinAngle = Mathf.Sin (angle);
+			float cosAngle = Mathf.Cos (angle);
+			Vector3 newpos = new Vector3 (cosAngle*transform.position.x-sinAngle*transform.position.z,0,
+				sinAngle*transform.position.x+cosAngle*transform.position.z);
+			transform.position = newpos;
+			transform.Rotate(new Vector3(0,-angle*180/Mathf.PI,0));
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			float angle = angularspeed*Time.deltaTime;
+			float sinAngle = Mathf.Sin (angle);
+			float cosAngle = Mathf.Cos (angle);
+			Vector3 newpos = new Vector3 (cosAngle*transform.position.x-sinAngle*transform.position.z,0,
+				sinAngle*transform.position.x+cosAngle*transform.position.z);
+			transform.position = newpos;
+			transform.Rotate(new Vector3(0,-angle*180/Mathf.PI,0));
+		}
+	
+
 	}
 }
